8a0d32e6a7295a49983961cdec9929ea5349e717
ShadowTimGit <trfreezelaw@gmail.com>
2025-08-29 11:26:23 -0500

    update

---
+++ src/test/java/com/example/SkullTimerPluginTest.java
@@ -0,0 +1,13 @@
+package com.skulltimer;
+
+import net.runelite.client.RuneLite;
+import net.runelite.client.externalplugins.ExternalPluginManager;
+
+public class SkullTimerPluginTest
+{
+    public static void main(String[] args) throws Exception
+    {
+        ExternalPluginManager.loadBuiltin(SkullTimerPlugin.class);
+        RuneLite.main(args);
+    }
+}
